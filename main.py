1f44332c219f4635ea21d8b9af2ebe2a5bba56d2
cjporter25 <chris.j.porter25@gmail.com>
2025-04-01 11:10:58 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+import difflib
+import tempfile
+import sounddevice as sd
+import wavio
+import whisper
+import pyttsx3
+import threading
+import tkinter as tk
+from tkinter import messagebox
